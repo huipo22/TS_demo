98c4b3cda2d81842b168d3d9cca68da33c592d97
huhuibo <huipo22@163.com>
2016-12-21 17:46:11 +0800

    module

--- fanxing/Generic01.ts
+++ fanxing/Generic01.ts
@@ -8,11 +8,52 @@
 // }
 
 /*数组有length属性*/
-function Hello<T>(num:T[]):T[]{
-    alert(num.length)
-    return num;
+// function Hello<T>(num:T[]):T[]{
+//     alert(num.length)
+//     return num;
+// }
+// var List:Array<string>=Hello<string>(["1","2","3"])
+// for(var i=0;i<List.length;i++){
+//     alert(List[i])
+// }
+
+// function Hello<T>(arg: T): T {
+//     return arg;
+// }
+// // var myHello: <K>(arg: K)=>K = Hello;
+// // alert(myHello("hello"))
+//
+// var myHello: {<T>(arg: T): T} = Hello;
+// alert(myHello('hello'))
+
+//
+// interface Hello {
+//     <T>(arg: T): T;
+// }
+// function myHello<T>(arg: T): T {
+//     return arg;
+// }
+// var MH:Hello=myHello;
+// alert(MH("hello"))
+
+//
+// interface Hello<T> {
+//     (arg: T): T;
+// }
+// function myHello<T>(arg: T): T {
+//     return arg;
+// }
+// var mh: Hello<number> = myHello;
+// alert(mh(100))
+
+class HelloNumber<T> {
+    Ten: T;
+    add: (x: T, y: T)=>T;
 }
-var List:Array<string>=Hello<string>(["1","2","3"])
-for(var i=0;i<List.length;i++){
-    alert(List[i])
+var myHelloNumber = new HelloNumber<number>();
+myHelloNumber.Ten = 10;
+myHelloNumber.add = function (x, y) {
+    return x + y;
 }
+alert(myHelloNumber.Ten)
+alert(myHelloNumber.add(10, 20))
---
+++ fanxing/lam.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Ad8888 on 2016/12/21.
+ */
+
+var myFunc: (a: number)=>string = function (a: number): string {
+    return 'Hello' + a;
+}
+alert(myFunc(2))
